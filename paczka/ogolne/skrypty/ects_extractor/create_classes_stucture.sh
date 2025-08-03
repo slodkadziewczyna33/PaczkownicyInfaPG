0fd48b14b9b285d188c61a20833853437baf33fa
Billypl <kriper01@wp.pl>
2025-08-04 01:15:22 +0200

    fix sem7

--- paczka/ogolne/skrypty/ects_extractor/create_classes_stucture.sh
+++ paczka/ogolne/skrypty/ects_extractor/create_classes_stucture.sh
@@ -27,7 +27,7 @@ create_subject_folders() {
         mkdir -p "$subject_dir/seminarium"
     fi
     
-    # Dodaj standardowe foldery, które mogą być potrzebne
+    # Dodaj standardowe foldery
     mkdir -p "$subject_dir/egzamin"
     mkdir -p "$subject_dir/opracowania"
     mkdir -p "$subject_dir/inne/książki"
@@ -47,18 +47,47 @@ while IFS= read -r line; do
     # Sprawdzamy czy linia to nowy semestr
     if [[ "$line" =~ ^SEMESTR ]]; then
         current_semester="$line"
-        current_semester=$(echo "$current_semester" | sed 's/ //; s/ //g')  # Usuwamy "SEMESTR" i spacje
+        current_semester=$(echo "$current_semester" | sed 's/ //; s/ //g')
         current_stream=""
         current_profile=""
         mkdir -p "$main_dir/$current_semester"
         
         # Dla semestrów 5 i 6 tworzymy folder Wspolne
-        if [[ "$current_semester" == "SEMESTR5" || "$current_semester" == "SEMESTR6" || "$current_semester" == "SEMESTR7" ]]; then
+        if [[ "$current_semester" == "SEMESTR5" || "$current_semester" == "SEMESTR6" ]]; then
             mkdir -p "$main_dir/$current_semester/Wspolne"
         fi
         continue
     fi
     
+    # SEMESTR7 - specjalna obsługa (pomijamy strumienie, tylko profile)
+    if [[ "$current_semester" == "SEMESTR7" ]]; then
+        # Sprawdzamy czy linia to profil
+        if [[ "$line" =~ ^[[:space:]]*(.*)\ \(Profil\) ]]; then
+            current_profile="${BASH_REMATCH[1]}"
+            current_profile=$(echo "$current_profile" | sed 's/^[[:space:]]*//; s/[[:space:]]*$//')
+            mkdir -p "$main_dir/$current_semester/$current_profile"
+            continue
+        fi
+        
+        # Sprawdzamy czy linia to przedmiot (zaczyna się od nawiasu)
+        if [[ "$line" =~ ^[[:space:]]*\( ]]; then
+            subject_code=$(echo "$line" | sed -E 's/^[[:space:]]*\(([^)]*)\).*/\1/')
+            subject_name=$(echo "$line" | sed -E 's/^[[:space:]]*\([^)]*\)_(.*):.*/\1/' | tr '_' ' ')
+            classes=$(echo "$line" | awk -F': ' '{print $2}')
+            folder_name="(${subject_code})_${subject_name// /_}"
+            
+            if [[ -n "$current_profile" ]]; then
+                full_path="${main_dir}/${current_semester}/${current_profile}/${folder_name}"
+            else
+                full_path="${main_dir}/${current_semester}/${folder_name}"
+            fi
+
+            create_subject_folders "$full_path" "$classes"
+        fi
+        continue
+    fi
+    
+    # Dla pozostałych semestrów normalna obsługa
     # Sprawdzamy czy linia to strumień
     if [[ "$line" =~ ^[[:space:]]*(.*)\ \(Strumień\) ]]; then
         current_stream="${BASH_REMATCH[1]}"
@@ -78,29 +107,23 @@ while IFS= read -r line; do
     
     # Sprawdzamy czy linia to przedmiot (zaczyna się od nawiasu)
     if [[ "$line" =~ ^[[:space:]]*\( ]]; then
-        # Wyciągamy skrót przedmiotu i nazwę
         subject_code=$(echo "$line" | sed -E 's/^[[:space:]]*\(([^)]*)\).*/\1/')
         subject_name=$(echo "$line" | sed -E 's/^[[:space:]]*\([^)]*\)_(.*):.*/\1/' | tr '_' ' ')
         classes=$(echo "$line" | awk -F': ' '{print $2}')
-        
-        # Tworzymy nazwę folderu
         folder_name="(${subject_code})_${subject_name// /_}"
         
-        # Tworzymy pełną ścieżkę
         if [[ -n "$current_profile" ]]; then
             full_path="${main_dir}/${current_semester}/${current_stream}/${current_profile}/${folder_name}"
         elif [[ -n "$current_stream" ]]; then
             full_path="${main_dir}/${current_semester}/${current_stream}/${folder_name}"
         else
-            # Dla semestrów 5 i 6 przedmioty bez strumienia trafiają do Wspolne
-            if [[ "$current_semester" == "SEMESTR5" || "$current_semester" == "SEMESTR6" || "$current_semester" == "SEMESTR7" ]]; then
+            if [[ "$current_semester" == "SEMESTR5" || "$current_semester" == "SEMESTR6" ]]; then
                 full_path="${main_dir}/${current_semester}/Wspolne/${folder_name}"
             else
                 full_path="${main_dir}/${current_semester}/${folder_name}"
             fi
         fi
 
-        # Tworzymy foldery
         create_subject_folders "$full_path" "$classes"
     fi
 done < "$1"
